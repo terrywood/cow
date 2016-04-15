e8bd0a969f1f53bda84add30324fca3c0e949ebf
terrywood <qq3532619@gmail.com>
2016-04-15 09:10:37 +0800

    json

--- src/test/java/ClientFormLogin.java
+++ src/test/java/ClientFormLogin.java
@@ -1,3 +1,6 @@
+import cn.skypark.code.MyCheckCodeTool;
+import org.apache.commons.codec.binary.StringUtils;
+import org.apache.commons.io.IOUtils;
 import org.apache.http.HttpEntity;
 import org.apache.http.client.methods.CloseableHttpResponse;
 import org.apache.http.client.methods.HttpGet;
@@ -7,16 +10,29 @@ import org.apache.http.cookie.Cookie;
 import org.apache.http.impl.client.BasicCookieStore;
 import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClients;
+import org.apache.http.impl.cookie.BasicClientCookie;
+import org.apache.http.impl.cookie.BasicClientCookie2;
 import org.apache.http.util.EntityUtils;
 
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
 import java.net.URI;
+import java.net.URL;
+import java.util.Date;
 import java.util.List;
 /**
  * Created by Administrator on 16-4-14.
  */
 public class ClientFormLogin {
     public static void main(String[] args) throws Exception {
+        long start = System.currentTimeMillis();
         BasicCookieStore cookieStore = new BasicCookieStore();
+ /*       Cookie cookie = new BasicClientCookie("JSESSIONID","B9A36160C47F08B46E037FAF638757A4");
+        Cookie cookie2 = new BasicClientCookie("pgv_pvi","595542016");
+        Cookie cookie3 = new BasicClientCookie("pgv_si","s7088572416");
+        cookieStore.addCookie(cookie);
+        cookieStore.addCookie(cookie2);
+        cookieStore.addCookie(cookie3);*/
         CloseableHttpClient httpclient = HttpClients.custom()
                 .setDefaultCookieStore(cookieStore)
                 .build();
@@ -42,18 +58,53 @@ public class ClientFormLogin {
                 response1.close();
             }
 
+            HttpGet httpget3 = new HttpGet("https://jy.yongjinbao.com.cn/winner_gj/gjzq/user/extraCode.jsp");
+            CloseableHttpResponse response3 = httpclient.execute(httpget3);
+
+
+            HttpEntity entity3 = response3.getEntity();
+            BufferedImage image = ImageIO.read(entity3.getContent());
+
+
+            EntityUtils.consume(entity3);
+
+
+          /*  URL url = new URL("https://jy.yongjinbao.com.cn/winner_gj/gjzq/user/extraCode.jsp");
+            BufferedImage image = ImageIO.read(url);
+*/
+            MyCheckCodeTool tool = new MyCheckCodeTool("guojin");
+            String code = tool.getCheckCode_from_image(image);
+
+            //String code = "2342";
+
+            System.out.println("very code["+code+"]");
             HttpUriRequest login = RequestBuilder.post()
-                    .setUri(new URI("https://someportal/"))
-                    .addParameter("IDToken1", "username")
-                    .addParameter("IDToken2", "password")
+                    .setUri(new URI("https://jy.yongjinbao.com.cn/winner_gj/gjzq/exchange.action"))
+                    .addParameter("validateCode", code)
+                    .addParameter("account_content", "40128457")
+                    .addParameter("password", "A%2B9BQUFnQUJBQUFRQk5mcSttVjdjRXJjN2FqRHhRNCtEZ0t4aTBZL3J1MFp4eW9idWx1bXJpSFdNOQ%3D%3D")
+                    .addParameter("function_id", "200")
+                    .addParameter("login_type", "stock")
+                    .addParameter("version", "200")
+                    .addParameter("identity_type", "")
+                    .addParameter("remember_me", "")
+                    .addParameter("input_content", "1")
+                    .addParameter("content_type", "0")
+                    .addParameter("loginPasswordType", "B64")
+                    .addParameter("disk_serial_id", "")
+                    .addParameter("mac_addr", "1C-39-47-20-CA-3A")
+                    .addParameter("cpuid", "-306D4-7FFAFBBF")
+                    .addParameter("machinecode", "-306D4-7FFAFBBF")
                     .build();
             CloseableHttpResponse response2 = httpclient.execute(login);
             try {
                 HttpEntity entity = response2.getEntity();
 
                 System.out.println("Login form get: " + response2.getStatusLine());
+                String result = IOUtils.toString(entity.getContent(), "UTF-8");     ;
                 EntityUtils.consume(entity);
 
+                System.out.println("result:" +result);
                 System.out.println("Post logon cookies:");
                 List<Cookie> cookies = cookieStore.getCookies();
                 if (cookies.isEmpty()) {
@@ -69,5 +120,8 @@ public class ClientFormLogin {
         } finally {
             httpclient.close();
         }
+
+        long end=  System.currentTimeMillis() - start;
+        System.out.println("use times :"+end);
     }
 }
---
+++ src/test/java/ReadImageWord.java
@@ -0,0 +1,25 @@
+import cn.skypark.code.MyCheckCodeTool;
+import com.sun.imageio.plugins.common.ImageUtil;
+import org.apache.commons.io.IOUtils;
+
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
+import java.io.IOException;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.text.ParseException;
+
+public class ReadImageWord {
+    public static void main(String[] args) throws ParseException, IOException {
+
+
+        MyCheckCodeTool tool = new MyCheckCodeTool("guojin");
+        URL url = new URL("https://jy.yongjinbao.com.cn/winner_gj/gjzq/user/extraCode.jsp");
+        BufferedImage image = ImageIO.read(url);
+        System.out.println("code=" + tool.getCheckCode_from_image(image));
+    }
+
+
+
+
+}
