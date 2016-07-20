b8f8cc76f6be7db3721cb9c437fa17efeb0b8b6d
terry.wu <terry.wu@gtomato.com>
2016-07-20 18:38:50 +0800

    优化

--- py/xueqiu.py
+++ py/xueqiu.py
@@ -9,7 +9,7 @@ import requests
 import hashlib
 import re
 import time
-
+from datetime import datetime
 # 构造 Request headers
 agent = 'Mozilla/5.0 (Windows NT 6.2; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/50.0.2661.102 Safari/537.36'
 headers = {
@@ -20,31 +20,66 @@ headers = {
     "Accept-Language": "zh-CN,zh;q=0.8,zh-TW;q=0.6",
     "Connection": "keep-alive"
 }
-
+telephone = "13660288080"
+password = "abcabc123123"
 session = requests.session()
-
+totalCount =0
+loginTimes=0
 
 # 密码的 md5 加密
 def get_md5(password):
     md5 = hashlib.md5()
     md5.update(password.encode())
     return md5.hexdigest().upper()
+def order(code,price,prev_weight_adjusted,target_weight):
 
-def getData():
+    pass
+
+def handleData(json):
+    #print(json)
+    now = datetime.now().timestamp()
+    for obj in json['list']:
+        if(obj['status'] == "success" or obj['status'] == "pending" ):
+            diffMin = (now -(obj['updated_at']/1000))/60/60
+            if(diffMin<30):
+                entity = obj['rebalancing_histories'][0]
+                order(entity['stock_id'],entity['price'],entity['prev_weight_adjusted'],entity['target_weight'])
+
+            #print(datetime.fromtimestamp(t))
+            #print(updateat/60/60)
+
+
+def heartbeat():
     x=0
+    global totalCount
     while True:
         x=x+1
-        r = session.get("https://xueqiu.com/cubes/rebalancing/history.json?cube_symbol=ZH902949&count=20&page=1",headers=headers)
+        r = session.get("https://xueqiu.com/cubes/rebalancing/history.json?cube_symbol=ZH902949&count=10&page=1",headers=headers)
         aa = r.json()
-        print(aa)
+        try:
+            _totalCount = aa['totalCount']
+            if totalCount!=_totalCount:
+                print("call change API -----")
+                totalCount=_totalCount
+                handleData(aa)
+            else:
+                print("no changeed -------------")
+        except:
+            print("error-------------------")
+            if(loginTimes>=10):
+                break
+            else:
+                login()
+
         time.sleep(5)
-        if x>0:
+        if x>10:
             break
-    print("finish")    
-        
+
+    print("finish")
+
 
 # 只写了手机号登录的情况，邮箱登录的情况，可以简单修改 postdata 后实现、
-def login(telephone, password):
+def login():
     url = 'https://xueqiu.com/'
     session.get(url, headers=headers)  # 访问首页产生 cookies
     headers['Referer'] = "https://xueqiu.com/"
@@ -61,14 +96,16 @@ def login(telephone, password):
     log = session.get("https://xueqiu.com/setting/user", headers=headers)
     pa = r'"profile":"/(.*?)","screen_name":"(.*?)"'
     res = re.findall(pa, log.text)
+    global loginTimes
     if res == []:
         print("登录失败，请检查你的手机号和密码输入是否正确")
+        loginTimes = loginTimes+1
     else:
         print('欢迎使用 xchaoinfo 写的模拟登录 \n 你的用户 id 是：%s, 你的用户名是：%s' % (res[0]))
-        getData()
+        loginTimes =0
+        heartbeat()
 
 
 if __name__ == '__main__':
-    telephone = "13660288080"
-    password = "abcabc123123"
-    login(telephone, password)
+    login()# -*- coding: utf-8 -*-
+
